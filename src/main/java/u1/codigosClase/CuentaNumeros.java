51ea1ed8c22d4f6407efa3985985c993ee8d7f83
seteruiz <mruiz29@educa.madrid.org>
2024-10-10 13:37:21 +0200

    7oct

--- src/main/java/u1/codigosClase/CuentaNumeros.java
+++ src/main/java/u1/codigosClase/CuentaNumeros.java
@@ -1,6 +1,7 @@
 package u1.codigosClase;
 
 public class CuentaNumeros {
+    
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.print(i + ", ");
--- src/main/java/u1/ejerciciosClase/LanzaLeeFichero.java
+++ src/main/java/u1/ejerciciosClase/LanzaLeeFichero.java
@@ -1,5 +1,8 @@
 package u1.ejerciciosClase;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class LanzaLeeFichero {
@@ -14,7 +17,24 @@ public class LanzaLeeFichero {
         //Ejecutar LeeFichero (debe ser un artefacto compilado) con el argumento de ejecución y redireccionar las E/S que necesitemos
         // Aquí no se puede usar ni File, ni FileReader.
 
+        String[] proceso = {"java", "-jar", "./out/artifacts/LeeFichero/LeeFichero.jar", f};
+        ProcessBuilder pb = new ProcessBuilder(proceso);
+        try {
+            //inheritIO redirecciona los 3 streams del subproceso: .in, .out, .err. a la consola actual
+            // Si hacemos inheritIO NO hacemos la redirección de p.getInputStream(), etc.
+            // Y al revés lo mismo, si redireccionamos p.getInputStream(), etc no podemos hacer inheritIO
+            //pb.inheritIO();
 
-
+            //La alternativa al inheritIO
+            Process p = pb.start();
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String linea = "";
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
